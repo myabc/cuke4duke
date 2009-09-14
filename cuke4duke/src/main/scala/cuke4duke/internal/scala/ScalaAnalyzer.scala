20321f017f8d3a6a41ee71fc2f1766fffebb93f4
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-09-15 01:13:42 +0200

    Explained why we need to look at a class, and not an instance to detect scala step definitions.

--- cuke4duke/src/main/scala/cuke4duke/internal/scala/ScalaAnalyzer.scala
+++ cuke4duke/src/main/scala/cuke4duke/internal/scala/ScalaAnalyzer.scala
@@ -8,6 +8,23 @@ class ScalaAnalyzer extends ClassAnalyzer {
   def registerHooksAndStepDefinitionsFor(clazz: Class[_], classLanguage: ClassLanguage) {
     if (classOf[ScalaDsl].isAssignableFrom(clazz)) {
 
+      /*
+      We get here before classes are registered in the objectFactory (PicoContainer/Spring).
+      Therefore, we can't ask the classLanguage for an instance of clazz yet.
+      (We'll get a NullPointerException from PicoFactory.getComponent())
+
+      Even if we changed ClassLanguage.load to objectFactory.addClass(clazz)
+      before invoking this method, we would run into problems if a class
+      with step definitions is abstract (they cannot be instantiated!)
+
+      Some people (using Java at least) actually define step definitions in
+      abstract classes. See Github ticket:
+      http://github.com/aslakhellesoy/cuke4duke/issues/closed#issue/15
+
+      Therefore - it should be possible to inspect step definitions of a class
+      *without* an instance - simply by looking at the clazz.
+      Is that possible?
+       */
       val scalaDsl = classLanguage.getTarget(clazz).asInstanceOf[ScalaDsl]
 
       for (stepDefinition <- scalaDsl.stepDefinitions)
